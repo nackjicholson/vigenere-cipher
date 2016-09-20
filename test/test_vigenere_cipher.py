2a33dc5c197f90ad5f46c190767610fab100ac76
Will Vaughn <willieviseoae@gmail.com>
2016-09-20 16:38:56 -0700

    test refactor, ci integration

--- test/test_vigenere_cipher.py
+++ test/test_vigenere_cipher.py
@@ -3,6 +3,7 @@ from vigenere_cipher.cipher import VigenereCipher, combine_character, extend_key
 
 class TestVigenereCipher(unittest.TestCase):
     def test_encode(self):
+        """should encode plaintext against keyword"""
         keyword = 'TRAIN'
         plaintext = 'ENCODEDINPYTHON'
         cipher = VigenereCipher(keyword)
@@ -10,10 +11,10 @@ class TestVigenereCipher(unittest.TestCase):
         actual = cipher.encode(plaintext)
         expected = 'XECWQXUIVCRKHWA'
 
-        msg = 'should encode plaintext against keyword'
-        self.assertEqual(actual, expected, msg=msg)
+        self.assertEqual(actual, expected)
 
     def test_encode_character(self):
+        """should encode single character against keyword"""
         keyword = 'TRAIN'
         plaintext = 'E'
         cipher = VigenereCipher(keyword)
@@ -21,10 +22,10 @@ class TestVigenereCipher(unittest.TestCase):
         actual = cipher.encode(plaintext)
         expected = 'X'
 
-        msg = 'should encode single character against keyword'
-        self.assertEqual(actual, expected, msg=msg)
+        self.assertEqual(actual, expected)
 
     def test_encode_spaces(self):
+        """should strip plaintext of spaces before encoding"""
         keyword = 'TRAIN'
         plaintext = 'ENCODED IN PYTHON'
         cipher = VigenereCipher(keyword)
@@ -32,10 +33,10 @@ class TestVigenereCipher(unittest.TestCase):
         actual = cipher.encode(plaintext)
         expected = 'XECWQXUIVCRKHWA'
 
-        msg = 'should strip plaintext of spaces before encoding'
-        self.assertEqual(actual, expected, msg=msg)
+        self.assertEqual(actual, expected)
 
     def test_encode_lowercase(self):
+        """should convert plaintext to uppercase before encoding"""
         keyword = 'TRAIN'
         plaintext = 'encoded IN PYTHON'
         cipher = VigenereCipher(keyword)
@@ -43,18 +44,21 @@ class TestVigenereCipher(unittest.TestCase):
         actual = cipher.encode(plaintext)
         expected = 'XECWQXUIVCRKHWA'
 
-        msg = 'should convert plaintext to uppercase before encoding'
-        self.assertEqual(actual, expected, msg=msg)
+        self.assertEqual(actual, expected)
 
 class TestCombineCharacter(unittest.TestCase):
     def test_combine_character(self):
+        """
+        should perform lookup of plain and
+        keyword chars to get cipher char
+        """
         self.assertEqual(combine_character('E', 'T'), 'X')
         self.assertEqual(combine_character('N', 'R'), 'E')
 
 class TestExtendKeyword(unittest.TestCase):
     def test_extend_keyword(self):
+        """should extend keyword by desired length"""
         actual = extend_keyword('TRAIN', 16)
         expected = 'TRAINTRAINTRAINT'
 
-        msg = 'should extend keyword by desired length'
-        self.assertEqual(actual, expected, msg=msg)
+        self.assertEqual(actual, expected)
--- vigenere_cipher/cipher.py
+++ vigenere_cipher/cipher.py
@@ -1,5 +1,3 @@
-
-
 def combine_character(plain, keyword):
     plain = plain.upper()
     keyword = keyword.upper()
