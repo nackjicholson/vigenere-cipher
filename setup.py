53a575975e44f68a1c7e72d01c46e36e9a4ea995
Will Vaughn <willieviseoae@gmail.com>
2016-09-20 17:34:03 -0700

    coverage

---
+++ setup.py
@@ -0,0 +1,11 @@
+import unittest
+from setuptools import setup, find_packages
+
+setup(
+    name='vigenere_cipher',
+    version='0.1.0',
+    author='Will Vaughn',
+    author_email='willieviseoae@gmail.com',
+    packages=find_packages(),
+    test_suite='test'
+)
